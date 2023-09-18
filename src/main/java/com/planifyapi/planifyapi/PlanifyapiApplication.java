85430dfaa31c869a5437f008512a7da1f9ad4396
Javier García Aguilar <javiergarciaaguilar2002@gmail.com>
2023-09-18 15:40:50 +0200

    Initial commit

---
+++ src/main/java/com/planifyapi/planifyapi/PlanifyapiApplication.java
@@ -0,0 +1,13 @@
+package com.planifyapi.planifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanifyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanifyapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planifyapi/planifyapi/PlanifyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planifyapi.planifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanifyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
