8df82d89e8f78d6e75370c2760a54c2b24e22bcf
JaviGarc1a <javgaragu1@alum.us.es>
2023-12-26 18:11:25 +0100

    feat: Add rate limit logic to fetchYoutubeData method

--- src/main/java/us/tfg/planifyapi/service/YoutubeService.java
+++ src/main/java/us/tfg/planifyapi/service/YoutubeService.java
@@ -1,15 +1,26 @@
 package us.tfg.planifyapi.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Service;
 import us.tfg.planifyapi.api.YoutubeApi;
 
 @Service
 public class YoutubeService {
 
-    // Lógica para interactuar con la API de YouTube
-    public String fetchYoutubeData() {
-        // Implementa la lógica para obtener datos de la API de YouTube aquí
-        YoutubeApi.callYoutube();
+    public String fetchYoutubeData(String rateLimit) {
+        Pattern pattern = Pattern.compile("(\\d+)/(\\d+)");
+        Matcher matcher = pattern.matcher(rateLimit);
+        int numCalls = 0;
+        if (matcher.find()) {
+            numCalls = Integer.parseInt(matcher.group(1));
+        }
+        
+        for(int i = 0; i < numCalls; i++) {
+            YoutubeApi.callYoutube("cancion de navidad", i + 1);
+            System.out.println("Llamada a la API de YouTube #" + (i + 1) + " realizada");
+        }
         return "Datos de YouTube obtenidos";
     }
 }
