7c106bd650ec1771c3fbede0cbde1c8a6b7107a5
JaviGarc1a <javgaragu1@alum.us.es>
2023-12-27 21:39:35 +0100

    docs: Generate javadoc on functions

--- src/main/java/us/tfg/planifyapi/Utils/Utils.java
+++ src/main/java/us/tfg/planifyapi/Utils/Utils.java
@@ -9,7 +9,13 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Utils {
-
+    
+    /**
+    * Creates a new file at the specified file path.
+    * If the file already exists, no action is taken.
+    *
+    * @param filePath the path of the file to be created
+    */
     public static void createFile(String filePath) {
         File file = new File(filePath);
         try {
@@ -21,13 +27,26 @@ public class Utils {
             e.printStackTrace();
         }
     }
-
+    
+    /**
+    * Parses the given content and returns a formatted string with additional information.
+    *
+    * @param content The content to be parsed.
+    * @param numCallApi The number of the API call.
+    * @return The parsed and formatted string.
+    */
     public static String parseContent(String content, int numCallApi) {
         String timeString = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-        String contentParsed = String.format("\n# Llamada a la API de YouTube #%d\n**Hora:** %s\n**Contenido:** \n\t**Título:** %s\n", numCallApi, timeString, content); 
+        String contentParsed = String.format("\n# Llamada a la API de YouTube #%d <br>\nHora: %s<br>\nContenido: <br>\nTítulo: %s<br>\n", numCallApi, timeString, content); 
         return contentParsed;
     }
-
+    
+    /**
+    * Writes content to a file specified by the given file path.
+    *
+    * @param filePath the path of the file to write to
+    * @param content  the content to write to the file
+    */
     public static void writeFile(String filePath, String content) {
         File file = new File(filePath);
         try {
@@ -40,7 +59,13 @@ public class Utils {
             e.printStackTrace();
         }
     }
-
+    
+    /**
+    * Reads the content of a file and returns it as a string.
+    *
+    * @param filePath the path of the file to be read
+    * @return the content of the file as a string
+    */
     public static String readFile(String filePath) {
         try {
             File file = new File(filePath);
@@ -51,19 +76,25 @@ public class Utils {
             }
             scanner.close();
             return content;
-
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
         return "";
     }
-
+    
+    /**
+    * Generates a daily report based on the given filename.
+    * The report includes the number of API calls made on that day.
+    *
+    * @param filename the name of the file used to generate the report
+    */
     public static void generateDailyReport(String filename) {
         String fileRoute = String.format("src/main/resources/report/%s-report.md", filename);
         String content = readFile(fileRoute);
         Pattern patternNumCall = Pattern.compile("#\\d+");
         String numCalls = String.valueOf(patternNumCall.matcher(content).results().count());
-        String report = String.format("# Reporte del día %s\n\n## Número de llamadas a la API \n%s", filename, numCalls);
+        String report = String.format("# Reporte del día %s ## Número de llamadas a la API \n%s", filename, numCalls);
         System.out.println("Reporte generado: \n" + report);
     }
     
