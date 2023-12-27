7760aadcb60c940197c67774420ddebbaa868e32
JaviGarc1a <javgaragu1@alum.us.es>
2023-12-27 22:02:31 +0100

    refactor: Refactor YoutubeApi class to include API key and handle quota errors

--- src/main/java/us/tfg/planifyapi/api/YoutubeApi.java
+++ src/main/java/us/tfg/planifyapi/api/YoutubeApi.java
@@ -1,63 +1,77 @@
 package us.tfg.planifyapi.api;
 
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import okhttp3.HttpUrl;
-import okhttp3.HttpUrl.Builder;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import us.tfg.planifyapi.Utils.Utils;
 
 public class YoutubeApi {
-
+    
     private static final String FILE_PATH = "src/main/resources/report";
-
-    public static void callYoutube(String query, int numCallApi) {
-       
+    
+    // La cuota de youtube se renueva todos los días a las 9:00 am
+    private static final LocalDateTime TIME_RENEW_QUOTA = LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(9, 0, 0));
+    
+    /**
+    * Calls the YouTube API to search for videos based on the given query.
+    * 
+    * @param query         the search query
+    * @param numCallsApi   the number of API calls to make
+    */
+    public static void callYoutube(String query, int numCallsApi) {
+        Request request = Utils.buildRequest("API_KEY_YOUTUBE", "https://www.googleapis.com/youtube/v3/search", query);
         OkHttpClient client = new OkHttpClient();
-        Dotenv dotenv = Dotenv.configure().load();
-        String apiKey = dotenv.get("API_KEY_YOUTUBE");
-        Builder urlBuilder = HttpUrl.parse("https://www.googleapis.com/youtube/v3/search").newBuilder();
-        urlBuilder.addQueryParameter("key", apiKey);
-        urlBuilder.addQueryParameter("part", "snippet");
-        urlBuilder.addQueryParameter("q", query); 
-
-        String url = urlBuilder.build().toString();
-
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-
-        try {
-            Response response = client.newCall(request).execute();
-            if (response.isSuccessful()) {
-                LocalDate date = LocalDate.now();
-                String filePath = String.format("%s/%s", FILE_PATH, date.toString() + ".md");
-                Utils.createFile(filePath);
         
-                String responseBody = response.body().string();
-                JSONObject jsonResponse = new JSONObject(responseBody);
-                JSONArray items = jsonResponse.getJSONArray("items");
-
-                for (int i = 0; i < items.length(); i++) {
-                    JSONObject item = items.getJSONObject(i);
-                    JSONObject snippet = item.getJSONObject("snippet");
-                    String videoTitle = snippet.getString("title");
-                    String parsedContent = Utils.parseContent(videoTitle, numCallApi);
-                    System.out.println(parsedContent);
-                    Utils.writeFile(filePath, parsedContent);
+        for (int numCallApi=0; numCallApi <= numCallsApi; numCallApi++) {
+            try {
+                Response response = client.newCall(request).execute();
+                String filePath = String.format("%s/%s", FILE_PATH, LocalDate.now().toString() + ".md");
+                if (response.isSuccessful()) {
+                    Utils.createFile(filePath);
+                    
+                    String responseBody = response.body().string();
+                    JSONObject jsonResponse = new JSONObject(responseBody);
+                    JSONArray items = jsonResponse.getJSONArray("items");
+                    
+                    // El response trae 5 videos, recorremos todos
+                    for (int i = 0; i < items.length(); i++) {
+                        JSONObject item = items.getJSONObject(i);
+                        JSONObject snippet = item.getJSONObject("snippet");
+                        String videoTitle = snippet.getString("title");
+                        String parsedContent = Utils.parseContent(videoTitle, numCallApi);
+                        System.out.println(parsedContent);
+                        Utils.writeFile(filePath, parsedContent);
+                    }
+                } else {
+                    System.out.println("Error en la solicitud: " + response.code());
+                    JSONObject jsonResponse = new JSONObject(response.body().string());
+                    JSONObject error = jsonResponse.getJSONObject("error");
+                    String message = error.getString("message");
+                    boolean isQuotaError = message.contains("quota");
+                    if (isQuotaError) {
+                        System.out.println("Se ha alcanzado el límite de llamadas a la API de YouTube");
+                        LocalDateTime timeNow = LocalDateTime.now();
+                        System.out.println("Hora actual: " + timeNow);
+                        System.out.println("Hora de renovación de la cuota: " + TIME_RENEW_QUOTA);
+                        Long durationRenew = Duration.between(timeNow, TIME_RENEW_QUOTA).toMillis();
+                        System.out.println("Esperando " + durationRenew + " milisegundos hasta renovar la cuota");
+                        Utils.generateDailyReport(filePath);
+                        Thread.sleep(durationRenew);
+                    }
                 }
-            } else {
-                System.out.println("Error en la solicitud: " + response.code());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
-
+    
     
 }
