f57a23ce1404c3b0718af4c74ee2d05e5434ddb3
JaviGarc1a <javgaragu1@alum.us.es>
2023-12-27 22:41:53 +0100

    feat: kill process spring

--- run.sh
+++ run.sh
@@ -4,6 +4,27 @@ PATH_PROJECT=~/dev/US-TFG-PlanifyAPI
 
 cd $PATH_PROJECT
 
+# Función para detener el proyecto Spring
+detener_proyecto() {
+    echo "Deteniendo el proyecto Spring..."
+    
+    # Obtener el PID del proceso del proyecto Spring
+    PID=$(ps aux | grep '[s]pring-boot:run' | awk '{print $2}')
+    
+    # Detener el proceso si se encuentra en ejecución
+    if [ -n "$PID" ]; then
+        kill "$PID"
+        echo "Proyecto Spring detenido."
+    else
+        echo "El proyecto Spring no está en ejecución."
+    fi
+
+    exit 0
+}
+
+# Capturar la señal SIGINT (Ctrl+C) y ejecutar la función detener_proyecto
+trap 'detener_proyecto' SIGINT
+
 # Ejecutar proyecto en segundo plano
 ./mvnw spring-boot:run &
 
@@ -11,3 +32,4 @@ cd $PATH_PROJECT
 sleep 10
 
 curl http://localhost:8080/api/youtube
+
